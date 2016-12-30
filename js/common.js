1efb82aea18b9138daa9edf5ea4a0c03ebf1f889
Roman Khairov <roman.khairov@gmail.com>
2016-12-30 23:09:27 +0100

    Add basic elements (in form of template) for site

---
+++ js/common.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+	$(".auth_buttons").click(function() {
+		$(this).next().slideToggle();
+	});
+
+
+	//coutdown timer
+	var austDay = new Date($(".countdown").attr("date-time"));
+	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
+
+	//manager FancyBox
+	$(".fancybox").fancybox();
+
+	//Landing Page navigation
+	$(".top_mnu").navigation();
+
+	//adding classes for .block animation
+	$(".block").waypoint(function(direction) {
+		if (direction === "down") {
+			$(".class").addClass("active");
+		} else if (direction === "up") {
+			$(".class").removeClass("deactive");
+		};
+	}, {offset: 100});
+
+	//smooth scroll
+	$("a.scroll").click(function() {
+		$.scrollTo($(".div"), 800, {
+			offset: -90
+		});
+	});
+
+	//Owl caousel
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 4
+	});
+	owl.on("mousewheel", ".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function(){
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function(){
+		owl.trigger("owl.prev");
+	});
+
+	//Button Top
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+	
+	//Ajax forms sending
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+
+});
