aaeafb73fb92204974476404e8dc6c0fa0388709
Roman Khairov <roman.khairov@gmail.com>
2017-01-01 20:20:42 +0100

    Add blog, add gradient color background, fixed menu slide

--- js/common.js
+++ js/common.js
@@ -3,23 +3,29 @@ $(document).ready(function() {
 	$(".auth_buttons").click(function() {
 		$(this).next().slideToggle();
 	});
-
-	$(".main_menu_button").click(function() {
-		$(".main_menu ul").slideToggle();
+	$(".main_mnu_button").click(function() {
+		$(".maian_mnu ul").slideToggle();
 	});
 
-
-	//coutdown timer
+	//Countdown
+	//Documentary: http://keith-wood.name/countdown.html
+	//<div class="countdown" date-time="2015-01-07"></div>
 	var austDay = new Date($(".countdown").attr("date-time"));
 	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
 
-	//manager FancyBox
+	//Pop-up FancyBox
+	//Documentary: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
 	$(".fancybox").fancybox();
 
-	//Landing Page navigation
+	//Landing Page Navigation
+	//$(".top_mnu") - это верхняя панель со ссылками.
+	//Ссылки вида <a href="#contacts">Контакты</a>
 	$(".top_mnu").navigation();
 
-	//adding classes for .block animation
+	//Add classes to child block .block for animation
+	//Documentary: http://imakewebthings.com/jquery-waypoints/
 	$(".block").waypoint(function(direction) {
 		if (direction === "down") {
 			$(".class").addClass("active");
@@ -28,20 +34,22 @@ $(document).ready(function() {
 		};
 	}, {offset: 100});
 
-	//smooth scroll
+	//Smooth skroll .scroll
+	//Documentary: https://github.com/flesler/jquery.scrollTo
 	$("a.scroll").click(function() {
 		$.scrollTo($(".div"), 800, {
 			offset: -90
 		});
 	});
 
-	//Owl caousel
+	//Carousel
+	//Documentary: http://owlgraphic.com/owlcarousel/
 	var owl = $(".carousel");
 	owl.owlCarousel({
 		items : 1,
 		autoHeight : true
 	});
-	owl.on("mousewheel", ".owl-wrapper", function (e) {
+	owl.on(/*"mousewheel",*/ ".owl-wrapper", function (e) {
 		if (e.deltaY > 0) {
 			owl.trigger("owl.prev");
 		} else {
@@ -49,14 +57,17 @@ $(document).ready(function() {
 		}
 		e.preventDefault();
 	});
-	$(".next_button").click(function(){
+	$(".next_button").click(function() {
 		owl.trigger("owl.next");
 	});
-	$(".prev_button").click(function(){
+	$(".prev_button").click(function() {
 		owl.trigger("owl.prev");
 	});
 
-	//Button Top
+	//"Top" button
+	//Documentary:
+	//http://api.jquery.com/scrolltop/
+	//http://api.jquery.com/animate/
 	$("#top").click(function () {
 		$("body, html").animate({
 			scrollTop: 0
@@ -64,14 +75,15 @@ $(document).ready(function() {
 		return false;
 	});
 	
-	//Ajax forms sending
-	$("form").submit(function() {
+	//sending forms Ajax 
+	//Documentary: http://api.jquery.com/jquery.ajax/
+	$("#callback").submit(function() {
 		$.ajax({
 			type: "GET",
 			url: "mail.php",
-			data: $("form").serialize()
+			data: $("#callback").serialize()
 		}).done(function() {
-			alert("Thank you!");
+			alert("Спасибо за заявку!");
 			setTimeout(function() {
 				$.fancybox.close();
 			}, 1000);
